9e9b62ebbf43ca11d610f395923c2735730c17a0
chaitanya <chaitanyagunjapalli@gmail.com>
2025-12-04 19:12:16 +0530

    creating a admin on deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-set -o errexit
-
+pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
-python manage.py collectstatic --no-input
-
-python manage.py migrate
+# Create superuser automatically if not exists
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'admin@example.com', 'Admin@123')" \
+| python manage.py shell
